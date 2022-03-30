684d71b3d17c2dcc0cba3d6a36c06a8cbc31f3ef
Rauno Moisto <rmoisto@gmail.com>
2022-03-30 12:11:32 +0300

    make it work on php 8

--- src/MultiCurl.php
+++ src/MultiCurl.php
@@ -3,90 +3,101 @@
 namespace MultiCurl;
 
 class MultiCurl {
-	private $mc;
-	private $running;
-	private $execStatus;
-	private $handlesNotSent = [];
+    private $mc;
+    private $running;
+    private $execStatus;
+    private $handlesNotSent = [];
+    private $handleId = 0;
+    private $handleIdMap = [];
 
-	public function __construct() {
-		$this->mc = curl_multi_init();
-	}
+    public function __construct() {
+        $this->mc = curl_multi_init();
+    }
 
-	public function addCurl($ch) {
-		$code = curl_multi_add_handle($this->mc, $ch);
+    public function addCurl($ch) {
+        $code = curl_multi_add_handle($this->mc, $ch);
 
-		if ($code === CURLM_OK || $code === CURLM_CALL_MULTI_PERFORM) {
-			do {
-				$this->execStatus = curl_multi_exec($this->mc, $this->running);
-			} while ($this->execStatus === CURLM_CALL_MULTI_PERFORM);
+        if ($code === CURLM_OK || $code === CURLM_CALL_MULTI_PERFORM) {
+            do {
+                $this->execStatus = curl_multi_exec($this->mc, $this->running);
+            } while ($this->execStatus === CURLM_CALL_MULTI_PERFORM);
 
-			$key = $this->getKey($ch);
-			$this->handlesNotSent[$key] = $ch;
-			return $key;
-		}
-		return null;
-	}
+            $key = $this->getKey($ch);
+            $this->handlesNotSent[$key] = $ch;
+            return $key;
+        }
+        return null;
+    }
 
-	public function sendRequest($key) {
-		if (isset($this->handlesNotSent[$key])) {
-			while (curl_getinfo($this->handlesNotSent[$key])['pretransfer_time'] === 0.0) {
-				usleep(2500);
-				$this->execStatus = curl_multi_exec($this->mc, $this->running);
-			}
-			unset($this->handlesNotSent[$key]);
-		}
-	}
+    public function sendRequest($key) {
+        if (isset($this->handlesNotSent[$key])) {
+            while (curl_getinfo($this->handlesNotSent[$key])['pretransfer_time'] === 0.0) {
+                usleep(2500);
+                $this->execStatus = curl_multi_exec($this->mc, $this->running);
+            }
+            unset($this->handlesNotSent[$key]);
+        }
+    }
 
-	public function sendAllRequests() {
-		foreach ($this->handlesNotSent as $key => $handle) {
-			$this->sendRequest($key);
-		}
-	}
+    public function sendAllRequests() {
+        foreach ($this->handlesNotSent as $key => $handle) {
+            $this->sendRequest($key);
+        }
+    }
 
-	public function getNextResult() {
-		if ($this->running) {
-			while ($this->running && ($this->execStatus == CURLM_OK || $this->execStatus == CURLM_CALL_MULTI_PERFORM)) {
-				usleep(2500);
-				curl_multi_exec($this->mc, $this->running);
+    public function getNextResult() {
+        if ($this->running) {
+            while ($this->running && ($this->execStatus == CURLM_OK || $this->execStatus == CURLM_CALL_MULTI_PERFORM)) {
+                usleep(2500);
+                curl_multi_exec($this->mc, $this->running);
 
-				$responses = $this->readResponses();
-				if ($responses !== null) {
-					return $responses;
-				}
-			}
-		} else {
-			return $this->readResponses();
-		}
+                $responses = $this->readResponses();
+                if ($responses !== null) {
+                    return $responses;
+                }
+            }
+        } else {
+            return $this->readResponses();
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	private function readResponses() {
-		$responses = [];
-		while ($done = curl_multi_info_read($this->mc)) {
-			$key = $this->getKey($done['handle']);
+    private function readResponses() {
+        $responses = [];
+        while ($done = curl_multi_info_read($this->mc)) {
+            $key = $this->getKey($done['handle']);
 
-			$done['response'] = curl_multi_getcontent($done['handle']);
-			$done['info'] = curl_getinfo($done['handle']);
-			$error = curl_error($done['handle']);
-			if ($error) {
-				$done['error'] = $error;
-			}
+            $done['response'] = curl_multi_getcontent($done['handle']);
+            $done['info'] = curl_getinfo($done['handle']);
+            $error = curl_error($done['handle']);
+            if ($error) {
+                $done['error'] = $error;
+            }
 
-			$responses[$key] = $done;
+            $responses[$key] = $done;
 
-			curl_multi_remove_handle($this->mc, $done['handle']);
-			curl_close($done['handle']);
-		}
+            curl_multi_remove_handle($this->mc, $done['handle']);
+            curl_close($done['handle']);
+        }
 
-		if (!empty($responses)) {
-			return $responses;
-		}
+        if (!empty($responses)) {
+            return $responses;
+        }
 
-		return null;
-	}
+        return null;
+    }
 
-	private function getKey($ch) : string {
-		return (string)$ch;
-	}
+    private function getKey($ch) : string {
+        foreach ($this->handleIdMap as $id => $handle) {
+            if ($handle === $ch) {
+                return (string)$id;
+            }
+        }
+
+        $id = ++$this->handleId;
+        $this->handleIdMap[$id] = $ch;
+
+        return (string)$id;
+    }
 }
